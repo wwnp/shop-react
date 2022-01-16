72e35437e5a818a5c016a25633de0bbaeb16d781
wwnp <iamtodaysad@gmail.com>
2022-01-16 13:30:40 +0300

    prep

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+export function App() {
+  return (
+    <div className="wrapper">
+      <Header></Header>
+      <main></main>
+      <Footer></Footer>
+    </div>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+export const Header = props => {
+  return (
+    <nav className='light-blue'>
+      <div className="nav-wrapper">
+        <a href="/" className="brand-logo">Logo</a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li><a href="sass.html">Sass</a></li>
+          <li><a href="badges.html">Components</a></li>
+          <li><a href="collapsible.html">JavaScript</a></li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
